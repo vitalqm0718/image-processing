d8743f3b1953991a0fdde95f5406b1e73601cd76
VITALQM <164301980+vitalqm0718@users.noreply.github.com>
2026-05-30 17:52:26 +0800

    Add files via upload

---
+++ GammaCorrection/main.m
@@ -0,0 +1,12 @@
+%% 对图像执行伽马矫正，支持灰度和彩色图像
+clc;clear;close all;
+%% 导入待去雾图像
+[fileName, pathName] = uigetfile({'*.jpg;*.png'}, 'Select image');
+img = imread([pathName, fileName]);
+%% 调用 gammaCorrection 函数处理
+gamma = 0.4;
+img_gc = gammaCorrection(img, gamma);
+%% 绘图
+figure;
+subplot(2,1,1);imshow(img);title('原图');
+subplot(2,1,2);imshow(img_gc);title('结果');
